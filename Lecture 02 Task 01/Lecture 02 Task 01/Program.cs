1fdb9d270108710931ff63c379ee63db74db5a1e
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 01:51:02 +0530

    Add files via upload

---
+++ Lecture 02 Task 01/Lecture 02 Task 01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_02_Task_01
+{
+    
+        public class Vehicle
+        {
+        
+            public string Brand { get; set; }
+            public int NumberOfWheels { get; set; }
+
+          
+            public Vehicle(string brand, int numberOfWheels)
+            {
+                Brand = brand;
+                NumberOfWheels = numberOfWheels;
+            }
+
+            
+            public void Sound()
+            {
+                switch (Brand.ToLower())
+                {
+                    case "car":
+                        Console.WriteLine("Vroom Vroom!");
+                        break;
+                    case "motorcycle":
+                        Console.WriteLine("Vroom!");
+                        break;
+                    case "bicycle":
+                        Console.WriteLine("Ring Ring!");
+                        break;
+                    default:
+                        Console.WriteLine("Beep Beep!");
+                        break;
+                }
+            }
+        }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            Vehicle myCar = new Vehicle("Car", 4);
+
+            Console.WriteLine($"Vehicle Brand: {myCar.Brand}");
+            Console.WriteLine($"Number of Wheels: {myCar.NumberOfWheels}");
+            Console.Write("Vehicle Sound: ");
+            myCar.Sound();
+        }
+    }
+}
+
