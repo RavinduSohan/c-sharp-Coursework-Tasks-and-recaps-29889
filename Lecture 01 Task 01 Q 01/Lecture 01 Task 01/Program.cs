0eea39d84f9ae7b3eef3e4b1b37247a0536770f8
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 01:48:56 +0530

    Add files via upload

---
+++ Lecture 01 Task 01 Q 01/Lecture 01 Task 01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        int baseNumber = 3;
+        int exponent = 4;
+
+      
+        if (exponent < 0)
+        {
+            Console.WriteLine("Exponent must be a non-negative integer.");
+            return; 
+        }
+
+        
+        int result = 1;
+        for (int i = 0; i < exponent; i++)
+        {
+            result *= baseNumber;
+        }
+        Console.WriteLine($"{baseNumber}^{exponent} = {result}");
+    }
+}
+
+
+
