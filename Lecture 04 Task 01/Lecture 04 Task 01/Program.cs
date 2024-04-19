987129d7673705350cebc88e57a236a7548b9db3
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 01:53:22 +0530

    Add files via upload

---
+++ Lecture 04 Task 01/Lecture 04 Task 01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Animal
+{
+    public void Speak()
+    {
+        Console.WriteLine("I am an Animal");
+    }
+}
+
+public class Dog : Animal
+{
+    public void DisplayLegs()
+    {
+        Console.WriteLine("I have four legs");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Dog myDog = new Dog();
+
+        
+        myDog.Speak();
+
+        
+        myDog.DisplayLegs();
+    }
+}
+
