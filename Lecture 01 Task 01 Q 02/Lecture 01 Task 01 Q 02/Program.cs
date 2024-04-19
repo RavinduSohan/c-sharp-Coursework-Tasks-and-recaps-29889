ca9ed8eb27170ba486ff7e11d462737c433f7a10
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 01:49:30 +0530

    Add files via upload

---
+++ Lecture 01 Task 01 Q 02/Lecture 01 Task 01 Q 02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Program
+{
+    public static char CheckOddOrEven(int number)
+    {
+        if (number % 2 == 0)
+        {
+            return 'E'; 
+        }
+        else
+        {
+            return 'O'; 
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        
+        Console.Write("Enter a number: ");
+        int userInput;
+
+        if (int.TryParse(Console.ReadLine(), out userInput))
+        {
+           
+            char result = CheckOddOrEven(userInput);
+
+            if (result == 'E')
+            {
+                Console.WriteLine($"{userInput} is even (E).");
+            }
+            else
+            {
+                Console.WriteLine($"{userInput} is odd (O).");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+}
+
