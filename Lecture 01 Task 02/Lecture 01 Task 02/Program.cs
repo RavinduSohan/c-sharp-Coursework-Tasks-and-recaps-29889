bbe656f9a05e0929da2cb767a83808ca3698737d
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 01:50:33 +0530

    Add files via upload

---
+++ Lecture 01 Task 02/Lecture 01 Task 02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_01_Task_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            
+            Console.Write("Enter your age: ");
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                
+                if (age >= 18)
+                {
+                    Console.WriteLine($"Hello, {name}! You are eligible to vote.");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello, {name}! You are not eligible to vote yet.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid age input. Please enter a valid integer.");
+            }
+        }
+    }
+}
